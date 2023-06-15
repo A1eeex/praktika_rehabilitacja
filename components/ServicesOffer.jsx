1ca21b4afb468681f4c07e2c8c66852004623b54
Alex <opetrovskyi423@gmail.com>
2023-06-15 16:03:14 +0200

    offer-fix

--- components/ServicesOffer.jsx
+++ components/ServicesOffer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import InfoCard from './atom/InfoCard';
+import PhysiotherapistContact from './atom/PhysiotherapistContact';
 
 const ServicesOffer = ({title}) => {
     return (
@@ -7,39 +8,42 @@ const ServicesOffer = ({title}) => {
             <div className="container m-auto">
                 <div className="text-white font-bold text-center text-4xl mb-12">{title}</div>
                 <div className={`flex flex-col lg:flex-row`}>
-                <div className='w-full lg:w-1/2 px-6 flex flex-col gap-5'>
-                    <InfoCard
-                        description={`Fizjoterapia ureginekologiczna kobiet i urologiczna mężczyzn`}
-                        listDescription={`
-                                            <li>Nietrzymanie moczu i zaburzenia dolnych dróg moczowych</li>
-                                            <li>Bóle menstruacyjne</li>
-                                            <li>Obniżenie narządów miednicy</li>
-                                            <li>Terapia w pochwicy, vulvodynii</li>
-                                            <li>Zespoły napięciowe</li>
-                                            <li>Bóle krocza kobiet i mężczyzn</li>
-                                            <li>Przygotowanie oraz rehabilitacja po zabiegu prostatektomii</li>
-                                        `}            
-                    />  
-                    <InfoCard description={'Elektrostymulacja mięśni dna miednicy'}/>  
-                    <InfoCard description={'Terapia manualna'}/>  
+                    <div className='w-full lg:w-1/2 px-6 flex flex-col gap-5'>
+                        <InfoCard
+                            description={`Fizjoterapia ureginekologiczna kobiet i urologiczna mężczyzn`}
+                            listDescription={`
+                                                <li>Nietrzymanie moczu i zaburzenia dolnych dróg moczowych</li>
+                                                <li>Bóle menstruacyjne</li>
+                                                <li>Obniżenie narządów miednicy</li>
+                                                <li>Terapia w pochwicy, vulvodynii</li>
+                                                <li>Zespoły napięciowe</li>
+                                                <li>Bóle krocza kobiet i mężczyzn</li>
+                                                <li>Przygotowanie oraz rehabilitacja po zabiegu prostatektomii</li>
+                                            `}            
+                        />  
+                        <InfoCard description={'Elektrostymulacja mięśni dna miednicy'}/>  
+                        <InfoCard description={'Terapia manualna'}/>  
+                    </div>
+                    <div className='w-full lg:w-1/2 px-6 flex flex-col gap-5'>
+                        <InfoCard
+                            description={`Fizjoterapia kobiet w ciąży i po porodzie`}
+                            listDescription={`
+                                                <li>Rozejście mięśnia prostego brzucha</li>
+                                                <li>Dolegliwości bólowe w obrębie kręgosłupa i stawach krzyżowo biodrowych</li>
+                                                <li>Mobilizacja blizn po cesarskim cięciu i porodzie naturalnym</li>
+                                                <li>Kinesiotaping</li>
+                                                <li>Profilaktyka poporodowa</li>
+                                            `}            
+                        /> 
+                        <InfoCard description={'EMG Biofeedback'}/>  
+                        <InfoCard description={'Masaż'}/>  
+                    </div>              
                 </div>
-                <div className='w-full lg:w-1/2 px-6 flex flex-col gap-5'>
-                    <InfoCard
-                        description={`Fizjoterapia kobiet w ciąży i po porodzie`}
-                        listDescription={`
-                                            <li>Rozejście mięśnia prostego brzucha</li>
-                                            <li>Dolegliwości bólowe w obrębie kręgosłupa i stawach krzyżowo biodrowych</li>
-                                            <li>Mobilizacja blizn po cesarskim cięciu i porodzie naturalnym</li>
-                                            <li>Kinesiotaping</li>
-                                            <li>Profilaktyka poporodowa</li>
-                                        `}            
-                    /> 
-                    <InfoCard description={'EMG Biofeedback'}/>  
-                    <InfoCard description={'Masaż'}/>  
-                </div>
-  
             </div>
-            </div>
+            <PhysiotherapistContact
+                fullName={"Katarzyna Rogóż"}
+                position={"mgr fizjoterapii"}
+            />
         </div>
     );
 };
--- components/TheFooter.jsx
+++ components/TheFooter.jsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const TheFooter = () => {
   return (
-  <footer className="container m-auto text-gray-600 body-font">
-    <div className="flex flex-col p-12 gap-12 lg:flex-row items-center lg:gap-0">
+  <footer className="text-gray-600 body-font">
+    <div className="container m-auto flex flex-col p-12 gap-12 lg:flex-row items-center lg:gap-0">
       <div className="w-full px-1 lg:w-1/3 ">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#e5097f" className="w-8 h-8 m-auto mb-5">
           <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
