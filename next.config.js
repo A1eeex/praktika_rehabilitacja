a771e1561cdb1067a9c2cd0e9107a160920a1099
Alex <opetrovskyi423@gmail.com>
2023-07-08 13:17:16 +0200

    info add

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: "export",  //Jeżeli chemy odpalić na locall to musimy zakomentować, przy npm run export włączone!!!
   // distDir: "build",
   // unoptimized: true,
   // experimental: {
